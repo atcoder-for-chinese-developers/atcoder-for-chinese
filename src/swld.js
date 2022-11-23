b94aa9257ab2f00a4e02a57005d468faeff6296d
d0j1a_1701 <dajia1701@gmail.com>
2022-11-23 22:20:23 +0800

    修复无限刷新

--- src/swld.js
+++ src/swld.js
@@ -1,3 +1,7 @@
 navigator.serviceWorker.register(`src/sw.js?time=${new Date().getTime()}`).then(async reg => {
-    window.location.search = `?time=${new Date().getTime()}`
+    if (window.localStorage.getItem('swinstall') != 'true') {
+        window.localStorage.setItem('swinstall', 'true');
+        window.location.search = `?time=${new Date().getTime()}`
+    }
+
 }).catch(err => { console.error(err); return; })
